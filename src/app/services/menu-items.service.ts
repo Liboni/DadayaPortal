981f8015ea9de3506a6f1433d54bb422e45c2fc4
Liboni T Sithole <libonis@afrosoft.co.zw>
2018-08-27 17:20:45 +0200

    done

--- src/app/services/menu-items.service.ts
+++ src/app/services/menu-items.service.ts
@@ -21,13 +21,13 @@ export class MenuItemsService {
           { name: 'Permissions', icon: 'ion-ios-people-outline', router: '/students', permission: Permission.None }
         ]
       },
-
       {
         name: 'Content Management',
         items: [
           { name: 'Events', icon: 'ion-ios-download-outline', router: '/events', permission: Permission.None },
           { name: 'Notices', icon: 'ion-ios-download-outline', router: '/notice', permission: Permission.None },
-          { name: 'Gallery', icon: 'ion-ios-download-outline', router: '/gallery', permission: Permission.None }
+          { name: 'Gallery', icon: 'ion-ios-download-outline', router: '/gallery', permission: Permission.None },
+          { name: 'Projects', icon: 'ion-ios-download-outline', router: '/projects', permission: Permission.None }
         ]
       }
     ];
